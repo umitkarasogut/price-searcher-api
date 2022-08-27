928848ba8ca16c96dab9c177f4b093d4fffdaf50
umitkarasogut <umtkrsgt@hotmail.com>
2022-08-27 17:59:13 +0300

    [refactor] code refactor

--- src/abstracts/abstract_provider.ts
+++ src/abstracts/abstract_provider.ts
@@ -1,5 +1,3 @@
-import { Product } from '@type/product.ts';
-
 export default abstract class AbstractProvider {
   protected crawl(url: string) {
     console.log(`%cFetching from ${url}`, 'color:yellow');
