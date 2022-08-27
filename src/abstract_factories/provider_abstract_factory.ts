0441eec3e33830137daa5eee4822095a8605af14
umitkarasogut <umtkrsgt@hotmail.com>
2022-08-27 17:56:30 +0300

    [refactor] code refactor

--- src/abstract_factories/provider_abstract_factory.ts
+++ src/abstract_factories/provider_abstract_factory.ts
@@ -1,22 +1,25 @@
-import TrendyolProvider from '@providers/trendyol.ts';
-import HepsiburadaProvider from '@providers/hepsiburada.ts';
+import Provider from '@interfaces/provider.ts';
 
 export default class ProviderAbstractFactory {
-  private providers = [
-    TrendyolProvider,
-    HepsiburadaProvider,
-  ];
+  private readonly providersPath = `${Deno.cwd()}/src/providers`;
 
   async search(string: string) {
+    const providerModules: Promise<{ default: new () => Provider }>[] = [];
 
-    const providerInstances = this.providers.map(providerInstance => new providerInstance());
+    for await (const provider of Deno.readDir(this.providersPath)) {
+      if (provider.isFile) {
+        providerModules.push(import(`${this.providersPath}/${provider.name}`));
+      }
+    }
 
-    const requests = providerInstances.map(providerInstance => providerInstance.setSearchString(string).search());
+    const providers = (await Promise.all(providerModules)).map(provider => new provider.default());
+
+    const requests = providers.map(providerInstance => providerInstance.setSearchString(string).search());
 
     const responses = await Promise.all(requests);
 
     responses.map((response, index) => console.log(
-      `%cFetched from ${providerInstances[index].providerName} with ${response.status} status code`,
+      `%cFetched from ${providers[index].providerName} with ${response.status} status code`,
       `color:${response.ok ? 'green' : 'red'}`
     ));
 
@@ -25,8 +28,8 @@ export default class ProviderAbstractFactory {
     );
 
     return htmls.map((html, index) => ({
-      provider: providerInstances[index].providerName,
-      products: providerInstances[index].processHtml(html),
+      provider: providers[index].providerName,
+      products: providers[index].processHtml(html),
     }));
   }
 }
--- src/interfaces/provider.ts
+++ src/interfaces/provider.ts
@@ -1,5 +1,11 @@
+import { Product } from '@type/product.ts';
+
 export default interface Provider {
+  providerName: string;
+
   setSearchString(string: string): Provider;
 
   search(): Promise<Response>;
+
+  processHtml(html: string): Product[];
 }
