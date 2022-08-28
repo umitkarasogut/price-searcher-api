85a6839f55b1f0bfedeeea5305945f3ae5b55b33
umitkarasogut <umtkrsgt@hotmail.com>
2022-08-29 00:58:10 +0300

    [feature] add oak to project

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
+import { Application } from "@oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import router from './router.ts';
+
+const app = new Application();
+
+app.use(oakCors());
+app.use(router.routes());
+
+console.info("CORS-enabled web server listening on port 8080");
+await app.listen({ port: 8080 });
+
+
+/*
 import ProviderAbstractFactory from './abstract_factories/provider_abstract_factory.ts';
+import { createHttpError } from 'std/http/http_errors.ts';
 import { serve } from 'std/http/server.ts';
+import { Status } from 'std/http/http_status.ts';
 
 const handler = async (request: Request): Promise<Response> => {
   try {
@@ -16,12 +32,18 @@ const handler = async (request: Request): Promise<Response> => {
     return new Response(JSON.stringify(results), {
       status: 200,
       headers: {
+        'Access-Control-Allow-Origin': '*',
         'content-type': 'application/json',
       },
     });
   } catch (error) {
-    return new Response(JSON.stringify(error.message), { status: 500 });
+    throw createHttpError(
+      Status.BadRequest,
+      error.message,
+      { expose: false }
+    );
   }
 };
 
 await serve(handler, { port: 8080 });
+*/
---
+++ src/router.ts
@@ -0,0 +1,29 @@
+import { Request, Response, Router } from "@oak/mod.ts";
+import ProviderAbstractFactory from '@abstract_factories/provider_abstract_factory.ts';
+
+const router = new Router();
+
+router.post('/', async ({ request, response }: { request: Request; response: Response }) => {
+    try {
+        const start = performance.now();
+
+        const { search } = await request.body().value
+
+        const results = await new ProviderAbstractFactory().search(search);
+
+        const diff = performance.now() - start;
+
+        console.log(`Responsed at ${diff}ms`);
+
+        response.status = 200;
+        response.body = results;
+        return;
+
+    } catch (error) {
+        response.status = 500;
+        response.body = error.message;
+        return;
+    }
+})
+
+export default router;
